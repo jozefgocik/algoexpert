5c5890054bd41b737e5f682ce760933745e1e2f8
jozefgocik <jozefgocik03@gmail.com>
2024-04-09 18:00:23 +0200

    Added maximizeExpression

--- hard/maximizeExpression.py
+++ hard/maximizeExpression.py
@@ -29,8 +29,27 @@
 
 # Solution 2
 def maximizeExpression(array):
-    # Write your code here.
-    return -1
+    # O(n) time / O(n) space
+    if len(array) < 4:
+        return 0
+
+    maxA = [array[0]]
+    for i in range(1, len(array)):
+        maxA.append(max(maxA[i - 1], array[i]))
+
+    maxAMinusB = [None, maxA[0] - array[1]]
+    for i in range(2, len(array)):
+        maxAMinusB.append(max(maxAMinusB[i - 1], maxA[i - 1] - array[i]))
+
+    maxAMinusBPlusC = [None,  None, maxAMinusB[1] + array[2]]
+    for i in range(3, len(array)):
+        maxAMinusBPlusC.append(max(maxAMinusBPlusC[i - 1], maxAMinusB[i - 1] + array[i]))
+
+    maxAMinusBPlusCMinusD = [None, None, None, maxAMinusBPlusC[2] - array[3]]
+    for i in range(4, len(array)):
+        maxAMinusBPlusCMinusD.append(max(maxAMinusBPlusCMinusD[i - 1], maxAMinusBPlusC[i - 1] - array[i]))
+
+    return maxAMinusBPlusCMinusD[-1]
 
 
 print(maximizeExpression([3, 6, 1, -3, 2, 7]))
