4bf7a3645e4851ab3d3982e3426b22a4503032cb
jg480hd <jozef.gocik@student.tuke.sk>
2024-06-17 08:58:58 +0200

    Added juiceBottling

--- hard/juiceBottling.py
+++ hard/juiceBottling.py
@@ -1,3 +1,16 @@
 def juiceBottling(prices):
-    # Write your code here.
-    return []
+    # O(n ^ 3) time / O(n ^ 2) space
+    maxProfit = [0] * len(prices)
+    solutions = [[]] * len(prices)
+    for quantity in range(len(prices)):
+        for dividingPoint in range(quantity + 1):
+            possibleProfit = maxProfit[quantity - dividingPoint] + prices[dividingPoint]
+
+            if possibleProfit > maxProfit[quantity]:
+                maxProfit[quantity] = possibleProfit
+                solutions[quantity] = [dividingPoint] + solutions[quantity - dividingPoint]
+
+    return solutions[-1]
+
+
+print(juiceBottling([0, 1]))
